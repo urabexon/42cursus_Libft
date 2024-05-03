b246398d9debf48eb60cd27357409990490eb599
Hiroki Urabe <hurabe@c1r22s8.42tokyo.jp>
2024-05-03 21:08:47 +0900

    add

--- ft_strrchr.c
+++ ft_strrchr.c
@@ -6,8 +6,24 @@
 /*   By: hurabe <hurabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:08:20 by hurabe            #+#    #+#             */
-/*   Updated: 2024/04/23 15:11:07 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/05/03 21:07:52 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+char	*strrchr(const char *s, int c)
+{
+	int	i;
+
+	i = 0;
+	while (s[i])
+		i++;
+	while (i >= 0)
+	{
+		if (s[i] == (char)c)
+			return ((char*)(s + 1));
+		i--;
+	}
+	return (NULL);
+}
