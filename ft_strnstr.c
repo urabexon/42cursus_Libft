0a5385ea8b8e5a3b94bbd0b1f5bbcd5b70c946ee
Hiroki Urabe <hurabe@c1r22s8.42tokyo.jp>
2024-05-11 20:34:16 +0900

    add

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: hurabe <hurabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:08:17 by hurabe            #+#    #+#             */
-/*   Updated: 2024/05/08 17:42:10 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/05/11 20:33:35 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,23 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
+	size_t	i;
+	size_t	j;
 
+	i = 0;
+	if (!haystack || !needle)
+		return (NULL);
+	if (needle[0] == '\0')
+		return ((char *)haystack);
+	while (haystack[i] && i < len)
+	{
+		j = 0;
+		while (haystack[i + j] && needle[j] \
+		&& i + j < len && haystack[i + j] == needle[j])
+			j++;
+		if (!needle[j])
+			return ((char *)(haystack + i));
+		i++;
+	}
+	return (NULL);
 }
