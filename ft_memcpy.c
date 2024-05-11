6c799c92c1ec6be15f1d3c305a285510567091c3
Hiroki Urabe <hurabe@c1r22s8.42tokyo.jp>
2024-05-11 21:26:01 +0900

    add

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,7 @@
 /*   By: hurabe <hurabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:07:14 by hurabe            #+#    #+#             */
-/*   Updated: 2024/05/08 17:38:37 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/05/11 21:25:44 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,19 @@
 
 void	*ft_memcpy(void *dst, const void *src, size_t n)
 {
-	
+	char		*dest_char;
+	const char	*src_char;
+	size_t		i;
+
+	dest_char = (char *)dst;
+	src_char = (const char *)src;
+	if (!dst || !src)
+		return (NULL);
+	i = 0;
+	while (i < n)
+	{
+		dest_char[i] = src_char[i];
+		i++;
+	}
+	return (dst);
 }
