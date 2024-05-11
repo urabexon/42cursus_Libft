b78d25756de0a37105426dec674dd9b7f232ea24
Hiroki Urabe <hurabe@c1r22s8.42tokyo.jp>
2024-05-11 17:59:16 +0900

    add

--- ft_strdup.c
+++ ft_strdup.c
@@ -6,7 +6,7 @@
 /*   By: hurabe <hurabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:07:47 by hurabe            #+#    #+#             */
-/*   Updated: 2024/05/08 17:40:19 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/05/11 17:58:45 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,25 @@
 
 char	*ft_strdup(const char *s1)
 {
-	
+	return (ft_substr(s1, 0, ft_strlen(s1)));
 }
+
+// int main(void) {
+//     char const *original = "Hello, world!";
+//     char *duplicate;
+
+//     // test1: Normal
+//     duplicate = ft_strdup(original);
+//     printf("Original: %s\n", original);
+//     printf("Duplicate: %s\n", duplicate);
+//     free(duplicate);
+
+//     // test2: empty
+//     original = "";
+//     duplicate = ft_strdup(original);
+//     printf("Original: \"%s\"\n", original);
+//     printf("Duplicate: \"%s\"\n", duplicate);
+//     free(duplicate);
+
+//     return (0);
+// }
