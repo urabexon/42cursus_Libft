df8bd07a1afe2e57898b4e22b9ceee6729907178
Hiroki Urabe <hurabe@c1r22s8.42tokyo.jp>
2024-05-13 18:58:21 +0900

    add

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: hurabe <hurabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:07:11 by hurabe            #+#    #+#             */
-/*   Updated: 2024/05/08 17:38:13 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/05/13 18:57:27 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,37 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	
+	unsigned char	*p1;
+	unsigned char	*p2;
+
+	if (n == 0)
+		return (0);
+	p1 = (unsigned char *)s1;
+	p2 = (unsigned char *)s2;
+	while (n--)
+	{
+		if (*p1 != *p2)
+			return (*p1 - *p2);
+		p1++;
+		p2++;
+	}
+	return (0);
 }
+
+// int main(void) {
+//     char str1[] = "Hello, world!";
+//     char str2[] = "Hello, world!";
+//     char str3[] = "Hello, World!";
+//     char str4[] = "Hello";
+
+//     printf("expected: 0, Got: %d\n", ft_memcmp(str1, str2, 13));
+
+//     printf("expected: non-zero, Got: %d\n", ft_memcmp(str1, str3, 13));
+
+//     printf("expected: 0, Got: %d\n", ft_memcmp(str1, str4, 5));
+
+//     char str5[] = "World, Hello!";
+//     printf("expected: non-zero, Got: %d\n", ft_memcmp(str1, str5, 13));
+
+//     return (0);
+// }
