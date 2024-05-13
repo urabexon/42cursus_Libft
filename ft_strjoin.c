d0f02c681450d9622017e0aab6187763f088652a
Hiroki Urabe <hurabe@c1r22s8.42tokyo.jp>
2024-05-13 19:47:05 +0900

    add

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: hurabe <hurabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:07:55 by hurabe            #+#    #+#             */
-/*   Updated: 2024/05/08 17:40:46 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/05/13 19:46:15 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,36 @@
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	
+	char	*str1;
+	char	*str2;
+
+	if (!s1 || !s2)
+		return (NULL);
+	str1 = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!str1)
+		return (NULL);
+	str2 = str1;
+	while (*s1)
+		*str2++ = *s1++;
+	while (*s2)
+		*str2++ = *s2++;
+	*str2 = '\0';
+	return (str1);
 }
+
+// int main(void) {
+//     char const *s1 = "Hello, ";
+//     char const *s2 = "world!";
+//     char *result;
+
+//     // test
+//     result = ft_strjoin(s1, s2);
+//     if (result) {
+//         printf("Test 1: %s\n", result);
+//         free(result);
+//     } else {
+//         printf("Test 1 failed to allocate memory.\n");
+//     }
+
+//     return (0);
+// }
