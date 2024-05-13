f81978dc61817ef2859423fa46bbf294a665c72a
Hiroki Urabe <hurabe@c1r22s8.42tokyo.jp>
2024-05-13 17:02:14 +0900

    add

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: hurabe <hurabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:07:17 by hurabe            #+#    #+#             */
-/*   Updated: 2024/05/08 17:38:52 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/05/13 16:57:05 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,28 @@
 
 void	*ft_memmove(void *dst, const void *src, size_t len)
 {
+	unsigned char	*d;
+	unsigned char	*s;
 
+	if (!dst || !src)
+		return (dst);
+	d = (unsigned char *)dst;
+	s = (unsigned char *)src;
+	if (d < s)
+	{
+		while (len--)
+		{
+			*d++ = *s++;
+		}
+	}
+	else
+	{
+		d += len;
+		s += len;
+		while (len--)
+		{
+			*--d = *--s;
+		}
+	}
+	return (dst);
 }
