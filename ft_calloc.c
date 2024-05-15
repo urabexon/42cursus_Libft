06b684edc14aec68e6524b9431527abf87926c09
Hiroki Urabe <hurabe@c1r22s8.42tokyo.jp>
2024-05-15 19:31:26 +0900

    fix

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: hurabe <hurabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:06:31 by hurabe            #+#    #+#             */
-/*   Updated: 2024/05/08 17:47:29 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/05/15 19:27:08 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,13 @@
 
 void	*ft_calloc(size_t count, size_t size)
 {
-	void	*ptr;
+	void	*ptr ;
 
-	ptr = malloc(size * count);
+	if (count && size > SIZE_MAX / count)
+		return (NULL);
+	ptr = (void *)malloc(count * size);
 	if (!ptr)
 		return (NULL);
-	ft_bzero(ptr, size * count);
+	ft_bzero(ptr, count * size);
 	return (ptr);
 }
