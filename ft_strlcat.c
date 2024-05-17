49d12c9856ad786c2818549fc25bcaf918bf3b6a
Hiroki Urabe <hurabe@c1r22s8.42tokyo.jp>
2024-05-17 19:56:37 +0900

    fix

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: hurabe <hurabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:08:00 by hurabe            #+#    #+#             */
-/*   Updated: 2024/05/08 18:39:50 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/05/17 19:55:28 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,10 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	count;
 	size_t	result;
 
-	dst_len = ft_strlen(dst);
+	if (dst == NULL)
+		dst_len = 0;
+	else
+		dst_len = ft_strlen(dst);
 	src_len = ft_strlen(src);
 	count = 0;
 	if (dstsize <= dst_len)
