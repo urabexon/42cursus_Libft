f9b0c3ace8ff55b3b6da16395df9cab7e6746fd6
Hiroki Urabe <hurabe@c1r22s8.42tokyo.jp>
2024-05-19 20:26:47 +0900

    add

--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: hurabe <hurabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 15:32:49 by hurabe            #+#    #+#             */
-/*   Updated: 2024/05/19 19:36:54 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/05/19 20:26:05 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,15 @@
 
 int	ft_lstsize(t_list *lst)
 {
-	
+	t_list	*current;
+	int		i;
+
+	i = 0;
+	current = lst;
+	while (current != NULL)
+	{
+		i++;
+		current = current->next;
+	}
+	return (i);
 }
