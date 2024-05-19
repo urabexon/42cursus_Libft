c866f01be9d6efb779e6454f853bae57506bfef0
Hiroki Urabe <hurabe@c1r22s8.42tokyo.jp>
2024-05-19 15:14:42 +0900

    fix

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,13 +6,13 @@
 /*   By: hurabe <hurabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/22 13:07:52 by hurabe            #+#    #+#             */
-/*   Updated: 2024/05/15 17:01:41 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/05/19 15:09:45 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	ft_striteri(char *s, void (*f)(unsigned int, char*))
+void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int	i;
 
