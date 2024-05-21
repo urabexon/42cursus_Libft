10441199a06644fbf4f1a53fdcc582df4746a085
Hiroki Urabe <hurabe@c1r22s8.42tokyo.jp>
2024-05-21 21:01:31 +0900

    fix

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -6,13 +6,30 @@
 /*   By: hurabe <hurabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 15:32:44 by hurabe            #+#    #+#             */
-/*   Updated: 2024/05/20 22:07:57 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/05/21 16:48:51 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
-// {
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*node;
 
-// }
+	if (!lst || !f)
+		return (NULL);
+	new = NULL;
+	while (lst)
+	{
+		node = ft_lstnew(f(lst->content));
+		if (!node)
+		{
+			ft_lstclear(&node, del);
+			return (NULL);
+		}
+		ft_lstadd_back(&new, node);
+		lst = lst->next;
+	}
+	return (new);
+}
