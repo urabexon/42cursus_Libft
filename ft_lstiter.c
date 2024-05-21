03fc54fe47579455010ee7f7203aae26e0bd490e
Hiroki Urabe <hurabe@c1r22s8.42tokyo.jp>
2024-05-21 15:42:14 +0900

    add

--- ft_lstiter.c
+++ ft_lstiter.c
@@ -6,13 +6,19 @@
 /*   By: hurabe <hurabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 15:32:36 by hurabe            #+#    #+#             */
-/*   Updated: 2024/05/20 22:07:52 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/05/21 15:41:27 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// void	ft_lstiter(t_list *lst, void (*f)(void *))
-// {
-
-// }
+void	ft_lstiter(t_list *lst, void (*f)(void *))
+{
+	if (!lst || !f)
+		return ;
+	while (lst != NULL)
+	{
+		f(lst->content);
+		lst = lst->next;
+	}
+}
