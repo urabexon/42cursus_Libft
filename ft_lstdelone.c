f5b0fbd02de14dc0e465ad6908d384b62423853c
Hiroki Urabe <hurabe@c1r22s8.42tokyo.jp>
2024-05-21 15:01:17 +0900

    add

--- ft_lstdelone.c
+++ ft_lstdelone.c
@@ -6,13 +6,16 @@
 /*   By: hurabe <hurabe@student.42tokyo.jp>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/19 15:32:32 by hurabe            #+#    #+#             */
-/*   Updated: 2024/05/20 22:07:48 by hurabe           ###   ########.fr       */
+/*   Updated: 2024/05/21 15:00:45 by hurabe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-// void	ft_lstdelone(t_list *lst, void (*del)(void *))
-// {
-
-// }
+void	ft_lstdelone(t_list *lst, void (*del)(void *))
+{
+	if (!lst || !del)
+		return ;
+	del(lst->content);
+	free(lst);
+}
